a31c47a2b106cc8edfe9c4a2faf096d7f9747e09
lsanessl <saness729@gmail.com>
2023-10-28 20:03:17 +0300

    header to change level.cpp

---
+++ functions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "records.h"
+#include "field.h"
+#include "console.h"
+//TODO add class to swap from GUI to console
+
+void start(console&, field&); //change level.cpp
+void Exit(console&);
